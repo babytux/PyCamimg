aeb904c7d86473a440f5cf36df14f279cf215b6b
babytux <babytux@babytux-laptop>
2010-12-28 21:47:32 +0100

    Database functionality

---
+++ src/pycamimg/core/db/ConfigurationDB.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''
+Copyright 2008, 2009, 2010 Hugo Párraga Martín
+
+This file is part of PyCamimg.
+
+PyCamimg is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+PyCamimg is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with PyCamimg.  If not, see <http://www.gnu.org/licenses/>.
+
+'''
+
+import sys
+import logging
+
+if hasattr(sys, "frozen"):
+    logging._srcfile = os.path.join("logging", "__init__.pyo")
+
+import pycamimg
+try:
+    __log__ = logging.getLogger(pycamimg.LogName)
+except:
+    __log__ = logging.getLogger()
+
+from pycamimg.core.db import Util
+from pycamimg.core.db import Constants
+from pycamimg.util.Singleton import Singleton
+
+class ConfigurationDB:
+    '''
+    @summary: Handle configuration of photo album from database.
+    @note: It's a singleton. 
+    '''
+    __metaclass__ = Singleton
+    
+    __photoFolder__ = None
+    '''
+    @summary: Store folder where photos are
+    '''
+
+    def __init__(self):
+        '''
+        @summary: Initialize class and load parameters from db.
+        '''
+        self.__load__()
+    
+    def __load__(self):
+        '''
+        @summary: Load parameters from database. 
+        '''
+        conn = Util.getConnection()
+        if (conn != None):
+            pass
+            curr = conn.cursor()
+            curr.execute('select key, value from %s' % Constants.TBL_PARAMS)
+            
+            pair = curr.fetchone()
+            while (pair != None):
+                __log__.debug('Read parameter %s with value %s' % (pair[0], pair[1]))
+                if (pair[0] == Constants.CONFIG_PARAM_PHOTO_DIR):
+                    self.__photoFolder__ = pair[1]                
+                pair = curr.fetchone()
+                
+            conn.commit()
+            curr.close()
+            conn.close()
+        else:
+            __log__.error('It can not get connection with database.')
+        
+    def save(self):
+        '''
+        @summary: Saves current configuration.
+        '''
+        conn = Util.getConnection()
+        if (conn != None):
+            queryString = 'insert or ignore into %s (key, value) values (?, ?)' % Constants.TBL_PARAMS
+            updateQueryString = 'update %s set value = ? where key = ?' % Constants.TBL_PARAMS
+            curr = conn.cursor()
+            try:
+                curr.execute(updateQueryString, (self.__photoFolder__, Constants.CONFIG_PARAM_PHOTO_DIR, ))
+                if (curr.rowcount == 0):
+                    curr.execute(queryString, (Constants.CONFIG_PARAM_PHOTO_DIR, self.__photoFolder__, ))                
+                conn.commit()
+            except Error, e:
+                __log__.error('An error was occurred when it was saving configuration into database. %s', e)
+                conn.rollback()
+            curr.close()
+        else:
+            __log__.error('It can not get connection with database.')
+        
+    def getPhotoFolder(self):
+        '''
+        @summary: Gets folder where photo album is.
+        '''
+        return self.__photoFolder__
+    
+    def setPhotoFolder(self, folder):
+        '''
+        @sumary: Sets folder where photo album is.
+        @param folder: Path of photo album.
+        '''
+        self.__photoFolder__ = folder
---
+++ src/pycamimg/core/db/Util.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''
+Copyright 2008, 2009, 2010 Hugo Párraga Martín
+
+This file is part of PyCamimg.
+
+PyCamimg is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+PyCamimg is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with PyCamimg.  If not, see <http://www.gnu.org/licenses/>.
+
+'''
+import sys
+import sqlite3
+import os.path
+import logging
+
+if hasattr(sys, "frozen"):
+    logging._srcfile = os.path.join("logging", "__init__.pyo")
+
+import pycamimg
+try:
+    __log__ = logging.getLogger(pycamimg.LogName)
+except:
+    __log__ = logging.getLogger()
+
+def getConnection() :
+    '''
+    @summary: Gets new connection with database.
+    @return: sqlite3.Connection object.
+    '''
+    path = os.path.join(__DB_FOLDER__, __DB_FILENAME__)
+    __log__.debug('Getting sqlite connection from %s' % path)
+    return sqlite3.connect(path)
+
+
+def generateDB():
+    '''
+    @summary: Generates db schema.
+    '''
+    sql = None
+    try:
+        h = open(__DB_SCRIPT_FILE__, 'r')
+        sql = u"".join(h.readlines())
+    except IOError, err:
+        __log__.error('An IO error was occurred when it was loading script db file [%s]. %s' (__DB_SCRIPT_FILE__, err))
+    except Exception, e:
+        __log__.error('An error was occurred when it was loading script db file [%s]. %s' (__DB_SCRIPT_FILE__, e))
+    
+    if (sql != None):
+        conn = getConnection()
+        if (conn != None):
+            cur = conn.cursor()
+            try:
+                cur.executescript(sql)
+                conn.commit()
+            except Error, e:
+                __log__.error('An error was occurred when it was generating database. %s' % e)
+                conn.rollback()
+            cur.close()
+        conn.close
+    else:
+        __log__.debug('It can not get script to generate database, or %s is empty' % __DB_SCRIPT_FILE__)
+        
+def checkDB(generates = False):
+    '''
+    @summary: Checks if database exists.
+    @param generates: True to generate database when it does not exist. 
+    '''
+    path = os.path.join(__DB_FOLDER__, __DB_FILENAME__)
+    if (os.path.exists(path)):
+        __log__.debug('DB file exists on %s' % path)
+        generates = True
+    else:
+        __log__.info('DB file do not exists on %s' % path)
+    
+    if (generates):
+        __log__.debug('It will generates db scheme on %s' % path)
+        generateDB()
+    
